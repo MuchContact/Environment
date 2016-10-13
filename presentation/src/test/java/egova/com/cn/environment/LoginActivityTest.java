b18af3f3a70b2c5e31bf89c78c83f0737538dea0
MuchContact <liuhengjiangis@foxmail.com>
2016-10-13 15:38:18 +0800

    init

---
+++ presentation/src/test/java/egova/com/cn/environment/LoginActivityTest.java
@@ -0,0 +1,11 @@
+package egova.com.cn.environment;
+
+import org.junit.Test;
+
+public class LoginActivityTest {
+    @Test
+    public void should_login_with_right_auth_info() throws Exception {
+
+
+    }
+}
