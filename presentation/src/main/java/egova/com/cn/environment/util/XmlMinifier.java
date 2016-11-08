2c6f774593c1d85053d19e1928f7b013a686a666
MuchContact <liuhengjiangis@foxmail.com>
2016-11-08 16:28:51 +0800

    remove xml minifior (no use)

--- presentation/src/main/java/egova/com/cn/environment/util/XmlMinifier.java
+++
@@ -1,9 +0,0 @@
-package egova.com.cn.environment.util;
-
-public class XmlMinifier {
-    public static String minify(String origin) {
-        return origin.replaceAll("\n", "")
-                .replaceAll("\t", "")
-                .replaceAll("\\s{2,}", "");
-    }
-}
